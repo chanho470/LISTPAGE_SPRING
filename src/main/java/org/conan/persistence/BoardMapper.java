91693c14c21c791f071172805b81ba9a706584bb
chanho470 <chanho470@naver.com>
2021-12-28 10:38:50 +0900

    테이블 삽입 삭제 수정 (BoardMapper , BoardMapperTest)

--- src/main/java/org/conan/persistence/BoardMapper.java
+++ src/main/java/org/conan/persistence/BoardMapper.java
@@ -8,4 +8,13 @@ import org.conan.domain.BoardVO;
 public interface BoardMapper {
 //	@Select("select * from tbl_board where bno > 0")
 	public List<BoardVO> getList();
+	
+	public void insert(BoardVO board);
+	
+	public BoardVO read(Long bno);
+
+	public int delete(Long bno);
+
+	public int update(BoardVO board);
+	
 }
--- src/test/java/org/conan/test/BoardMapperTest.java
+++ src/test/java/org/conan/test/BoardMapperTest.java
@@ -1,5 +1,6 @@
 package org.conan.test;
 
+import org.conan.domain.BoardVO;
 import org.conan.persistence.BoardMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,5 +21,33 @@ public class BoardMapperTest {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		mapper.insert(board);
+		log.info(board);
+	}
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(1L);
+		log.info(board);
+	}
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT :" + mapper.delete(9L));
+	}
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(2L);
+		board.setTitle("수정한 제목");
+		board.setContent("수정한 내용");
+		board.setWriter("rose");
+		int count = mapper.update(board);
+		log.info("Update Count : " + count);
 	}
 }
