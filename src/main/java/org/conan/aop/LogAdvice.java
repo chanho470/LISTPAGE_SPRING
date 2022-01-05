f403252a83ef86db4054f47c28b73a0d38068a7f
chanho470 <chanho470@naver.com>
2022-01-05 11:19:19 +0900

    트랜잭션 설정 및 테스트

--- src/main/java/org/conan/aop/LogAdvice.java
+++ src/main/java/org/conan/aop/LogAdvice.java
@@ -1,6 +1,10 @@
 package org.conan.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -24,5 +28,19 @@ public class LogAdvice {
 	public void logException(Exception exception) {
 		
 		log.info("예외발생 !!!!  ....."+exception);
+	}
+	@Around("execution(* org.conan.service.SampleService*.*(..))")
+	public Object logTime(ProceedingJoinPoint pjp) {
+		long start =System.currentTimeMillis();
+		log.info("target ====== "+pjp.getTarget());
+		log.info("Parm ========= "+Arrays.toString(pjp.getArgs()));
+		Object result = null;
+		
+		try {
+			result = pjp.proceed();
+		}catch(Throwable e) {}
+			long end = System.currentTimeMillis();
+			log.info("Time  ;;;;;" + (end - start));
+		return result;
 	}
 }
---
+++ src/test/java/org/conan/test/SampleServiceTxTest.java
@@ -0,0 +1,27 @@
+package org.conan.test;
+
+import org.conan.service.SampleTxService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class SampleServiceTxTest {
+	@Setter(onMethod_ = {@Autowired})
+	private SampleTxService service;
+	@Test 
+	public void testLong() {
+		String str = "나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n"+
+				"나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n 나는 김찬호 \n";
+		
+		log.info(str.getBytes().length);
+		service.addData(str);
+	}
+}
